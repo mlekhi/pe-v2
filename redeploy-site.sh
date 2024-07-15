d1009fdb97fc5e98fca5bdb524436355ad82754d
Maya <121539073+mlekhi@users.noreply.github.com>
2024-07-15 14:14:56 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
-# Check if the tmux session 'myflaskapp' exists and kill it if it does
-session_exists=$(tmux list-sessions | grep myflaskapp)
-if [ -n "$session_exists" ]; then
-    tmux kill-session -t myflaskapp
-fi
+# change directory to project folder
+cd pe-portfolio
 
-# Change directory to project folder
-cd pe-v2
-
-# Fetch latest changes from main branch
+# fetch latest changes from main branch
 git fetch
 
-# Reset the local repository to the latest changes from the main branch
+# reset the local repository to the latest changes from the main branch
 git reset origin/main --hard
 
-# Activate Python venv and install dependencies
+# activate Python venv and install dependencies
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# Start new detached Tmux session named 'myflaskapp'
-tmux new-session -d -s myflaskapp
-
-# Change directory to project folder within Tmux session
-tmux send-keys -t myflaskapp "cd pe-v2" Enter
-
-# Activate Python venv within Tmux session
-tmux send-keys -t myflaskapp "source python3-virtualenv/bin/activate" Enter
-
-# Start Flask server within Tmux session
-tmux send-keys -t myflaskapp "flask run --host=0.0.0.0" Enter
+# restart portfolio service
+sudo systemctl daemon-reload
+sudo systemctl restart myportfolio
