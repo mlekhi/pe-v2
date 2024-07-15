e4705d1c70ea8798cbdf88c34f49d1965041f5e9
Maya <121539073+mlekhi@users.noreply.github.com>
2024-07-15 00:18:10 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# kill all existing tmux sessions
-tmux kill-session -a
+# Check if the tmux session 'myflaskapp' exists and kill it if it does
+session_exists=$(tmux list-sessions | grep myflaskapp)
+if [ -n "$session_exists" ]; then
+    tmux kill-session -t myflaskapp
+fi
 
-# change directory to project folder
+# Change directory to project folder
 cd pe-v2
 
-# fetch latest changes from main branch
+# Fetch latest changes from main branch
 git fetch
 
-# Step 4: Reset the local repository to the latest changes from the main branch
+# Reset the local repository to the latest changes from the main branch
 git reset origin/main --hard
 
-# activate Python venv and install dependencies
+# Activate Python venv and install dependencies
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# start new detached Tmux session
+# Start new detached Tmux session named 'myflaskapp'
 tmux new-session -d -s myflaskapp
 
-# change directory to project folder within Tmux session
+# Change directory to project folder within Tmux session
 tmux send-keys -t myflaskapp "cd pe-v2" Enter
 
-# activate Python venv within Tmux session
+# Activate Python venv within Tmux session
 tmux send-keys -t myflaskapp "source python3-virtualenv/bin/activate" Enter
 
-# start Flask server within Tmux session
-tmux send-keys -t myflaskapp "flask run host=0.0.0.0" Enter
+# Start Flask server within Tmux session
+tmux send-keys -t myflaskapp "flask run --host=0.0.0.0" Enter
