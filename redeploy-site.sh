8b2a32ab7d73ad2a911cffd92db96781f783ef04
Maya <maya.lekhi1@gmail.com>
2024-07-26 16:27:15 -0400

    redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# change directory to project folder
-cd pe-portfolio
-
-# fetch latest changes from main branch
-git fetch
-
-# reset the local repository to the latest changes from the main branch
-git reset origin/main --hard
-
-# activate Python venv and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-# restart portfolio service
-sudo systemctl daemon-reload
-sudo systemctl restart myportfolio
+cd /path/to/your/project
+git fetch && git reset origin/main --hard
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
