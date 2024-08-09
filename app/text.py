5070956a2b095c5e22d073a358637a208a356041
Maya <maya.lekhi1@gmail.com>
2024-08-09 17:00:52 -0400

    testing actions

--- app/text.py
+++ app/text.py
@@ -9,7 +9,7 @@ work_text_maya = [
     "Crafted intuitive and user-friendly mobile app interfaces using HTML and CSS incorporating emerging design trends from social media platforms. Wireframed ~20 frames for app prototype to be presented to potential investors.",
 ]
 about_text_maya = [
-    "Sophomore at Western University studying Business Administration and Computer Science. Innovative software developer with a passion for creation and experience in fullstack & mobile development."
+    "Junior at Western University studying Business Administration and Computer Science. Innovative software developer with a passion for creation and experience in fullstack & mobile development."
 ]
 
 education_text_maya = [
